0b0dd6a9cb6a3a476fc1e4ded4b21bc66acdd002
benoit <oldering@gmail.com>
2017-03-30 10:04:13 +0900

    latest code

--- app/src/main/java/dagger/demo/DemoApplicationComponent.java
+++ app/src/main/java/dagger/demo/DemoApplicationComponent.java
@@ -7,8 +7,10 @@ import dagger.android.AndroidInjectionModule;
  * Created by gak on 9/25/14.
  */
 @Component(modules = {
-    ApplicationModule.class, AndroidInjectionModule.class, DemoActivityModule.class,
-    Demo2ActivityModule.class,
+    ApplicationModule.class, //
+    AndroidInjectionModule.class, //
+    DemoActivityModule.class, //
+    Demo2ActivityModule.class, //
 }) interface DemoApplicationComponent {
   DemoApplication injectApplication(DemoApplication application);
 }
